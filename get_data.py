e1215601e0ab88a3c1ff136de80b032cc4ee5da3
ashleshbhat <ashleshbhat@gmail.com>
2018-04-24 01:37:17 +0200

    data API functions done

--- get_data.py
+++ get_data.py
@@ -14,7 +14,9 @@ import os
 import numpy as np
 
 #=====================================================
-
+"""All data recieved from this is stored in a zip file in google drive at 
+https://drive.google.com/open?id=1eS2CROcg1sbjUuRLnLSrkSVVGhTqMAT3
+"""
 
 
 def get_stock(stock_name ="AAPL",series="daily",data_type="csv",output_size="compact",interval ="1",print_to_file=1):
@@ -80,41 +82,41 @@ def get_all_companies():
 #get_all_companies()
 
 def parse_intra_day():
-    filename = "data/test_companies.csv"
+    """This function takes chunks of intra day data and converts to csv file for each day stored in th
+    folder days under each stock folder
+    """
+    filename = "data/NASDAQ_companies.csv"
     with open(filename,"r",newline= '') as file:
         reader = csv.reader(file, lineterminator = '\n')
         company_data = list(reader)
     
     company_data = np.array(company_data)
-    for row1 in range (0,1): #len(company_data)
+    for row1 in range (300,800): #len(company_data)
         print("working on "+company_data[row1][0])
         filename_intraday = "data/"+company_data[row1][0]+"/"+company_data[row1][0]+"_intraday_full_1.csv"
         with open(filename_intraday,"r",newline= '') as file:
             reader = csv.reader(file,   lineterminator = '\n')
             company_data_single = list(reader)
             current_date = str(company_data_single[0][0]).split(" ")[0]
+            if(current_date == "\t<html>"): #error pages
+                continue
             current_data = []
-            row_current = 0
-            for row in range(0,5):#len(company_data_single)
+            
+            for row in range(0,len(company_data_single)):
                 read_date = str(company_data_single[row][0]).split(" ")[0]
-                print(read_date)
+                #print(read_date)
                 if(read_date == current_date):
                     current_data.append(company_data_single[row])
+                        
+                else:
+                    if(read_date != current_data) or (row == len(company_data_single)-1):
+                        
+                        date_filename = "data/"+company_data[row1][0]+"/date/"+current_date+".csv"
+                        os.makedirs(os.path.dirname(date_filename), exist_ok=True)
+                        with open(date_filename, 'w',newline='') as myfile:
+                            write = csv.writer(myfile, quoting=csv.QUOTE_NONE,escapechar=',')
+                            write.writerows(current_data)
+                        
+                        current_date = str(company_data_single[row][0]).split(" ")[0]
+                        current_data = []
                     
-                    row_current +=1
-                date_filename = "data/"+company_data[row1][0]+"/date/"+current_date+".csv"
-                print(date_filename)
-                os.makedirs(os.path.dirname(date_filename), exist_ok=True)
-                with open(date_filename, 'w',newline='') as myfile:
-                    write = csv.writer(myfile, quoting=csv.QUOTE_NONE)
-                    write.writerow(current_data[0])
-                    write.writerow(current_data[0])
-                    
-                  
-            print((current_data))
-                #filename = "data/"+company_data[row][0]+"/"+"intra_day_split/"+date+"."+data_type
-                #os.makedirs(os.path.dirname(filename), exist_ok=True)
-
-
-
-parse_intra_day()
