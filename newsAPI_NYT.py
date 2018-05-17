11d35e187e7aa996379a94214fb27bdd36bec7ac
ashleshbhat <ashleshbhat@gmail.com>
2018-05-17 19:48:13 +0200

    NYT news data added

--- newsAPI_NYT.py
+++ newsAPI_NYT.py
@@ -9,7 +9,7 @@ import datetime
 import time
 
 
-def get_NYT_data(querry ,begin_date,end_date):
+def get_NYT_data(querry,begin_date,end_date):
     url = ('https://api.nytimes.com/svc/search/v2/articlesearch.json?'
     '&api-key=3db8810edc4847fb895660fffe514214'
     '&q='+querry+
@@ -18,7 +18,7 @@ def get_NYT_data(querry ,begin_date,end_date):
     '&end_date='+end_date+
     '&fl=snippet,abstract,lead_paragraph'
     )
-    print(url)
+    #print(url)
     response = requests.get(url)
     data = (response.json())
     
@@ -26,16 +26,42 @@ def get_NYT_data(querry ,begin_date,end_date):
     docs = response['docs']
 
     #print(response)
+    output = ""
+    news_count= 0
     for news in docs:
         if 'snippet' in news:
             snippet = news['snippet']
+            output = output + snippet
+            
         if 'abstract' in news:
             abstract = news['abstract']
+            output = output + abstract
+            
         if 'lead_paragraph' in news:    
             lead_paragraph = news['lead_paragraph']
-        print(snippet)
-        print(abstract)
-        print()
-    #for news in data[]
+            output = output + lead_paragraph
+        
+        news_count = news_count + 1
+            
+    return output,news_count
+        
 
-get_NYT_data("apple","20170312","20170313")
+
+def get_NYT_multiple_weeks(number_of_weeks):
+    date_end = datetime.datetime(2003,4,20)
+    for loop in range (0, number_of_weeks):
+        date_begin = date_end - datetime.timedelta(days = 6)
+        date_end_parsed = str(date_end).replace("-","")[0:8]
+        date_begin_parsed = str(date_begin).replace("-","")[0:8]
+        print("working on the week "+str(date_begin)+" to "+str(date_end))
+        out,count = get_NYT_data("apple iphone,apple ipad,apple,apple ipod,macbook,macintosh",date_begin_parsed,date_end_parsed)
+        filename = "AAPL/NYT/"+date_begin_parsed+"_"+date_end_parsed+".txt"
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
+        out_file = open(filename,"w")
+        out_file.write(str(out.encode("utf-8")))
+        out_file.close()
+        
+        date_end = date_end - datetime.timedelta(days = 7)
+        time.sleep(0.5)
+
+get_NYT_multiple_weeks(600)
