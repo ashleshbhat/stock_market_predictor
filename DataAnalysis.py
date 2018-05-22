13dea03cbc6ffd2230a60e295715e7cf339f1fd1
Tobias <tobias.joschko@gmx.de>
2018-05-22 18:17:33 +0200

    added news and trained the classification model

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -55,7 +55,10 @@ def load_stock_data_weekly(company="AAPL"):
     return pd.read_csv(data_filename, parse_dates=['timestamp'])
 
 def load_news(company="AAPL"):
-    return pd.read_csv(company+"/news_Apple.csv", parse_dates=['date'])
+    return pd.read_csv(company+"/NYT/newsinfo.csv", parse_dates=['date begin'])
+
+def load_file(file="", dateString=""):
+    return pd.read_csv(file, parse_dates=[dateString])
 
 def playground():
     stock_data = load_stock_data_daily("AAPL")
@@ -88,7 +91,7 @@ def normalizeData(df):
     df['open'] = min_max_scaler.fit_transform(df.open.values.reshape(-1,1))
     df['high'] = min_max_scaler.fit_transform(df.high.values.reshape(-1,1))
     df['low'] = min_max_scaler.fit_transform(df.low.values.reshape(-1,1))
-    df['close'] = min_max_scaler.fit_transform(df.close.values.reshape(-1,1))
+    df['adjusted close'] = min_max_scaler.fit_transform(df.close.values.reshape(-1,1))
     return df
 def plot_bargraph(x, _data, _label="no_label", frame="weekly",_color="red"):
     #plot data
@@ -100,7 +103,9 @@ def plot_bargraph(x, _data, _label="no_label", frame="weekly",_color="red"):
     #set ticks per selection
     if(frame == "monthly"):
         ax.xaxis.set_major_locator(mdates.MonthLocator())
-    else:
+    elif (frame == "yearly"):
+        ax.xaxis.set_major_locator(mdates.YearLocator())
+    elif(frame == "weekly"):
         ax.xaxis.set_major_locator(mdates.WeekdayLocator())
     #set major ticks format
     ax.xaxis.set_major_formatter(mdates.DateFormatter('%b %d'))
@@ -128,11 +133,11 @@ def create_nn_model(layers, neurons, d):
 def training(file, k=5):
     # print (stockData.info())
     stockData = pd.read_csv(file)
-    stockData.dropna()
+    stockData = stockData.dropna()
     # drop the columns open, high and low
-    # stockData.drop("open", axis=1, inplace=True)
-    # stockData.drop("high", axis=1, inplace=True)
-    # stockData.drop("low", axis=1, inplace=True)
+    stockData.drop("open", axis=1, inplace=True)
+    stockData.drop("high", axis=1, inplace=True)
+    stockData.drop("low", axis=1, inplace=True)
     stockData.drop("dividend amount", axis=1, inplace=True)
 
     # save features to X
@@ -184,26 +189,6 @@ def process_stock(_stock="AAPL", _print=False, _type="compact",freq="daily" ,_lo
     # add ROC-1
     stockData = TI.rate_of_change(stockData,2)
 
-    # check if news information shall be loaded for the stock
-    if(_loadnews):
-        appleNews = load_news(_stock)
-        appleNews.set_index('date', inplace=True)
-        # create rating column and normalize values
-        appleNews['rating'] = (appleNews.positive - appleNews.negative)/(appleNews.positive + abs(appleNews.negative))
-        appleNews.info()
-        if(_print): 
-            # print news classification
-            plot_bargraph(appleNews.index, appleNews.rating, "newsClassification", "weekly")
-
-        # add news rating as colum to stockdata
-        stockData['newsRating'] = appleNews['rating']
-
-        stockData.info()
-        stockData.corr() # see and plot correlations 
-        if(_print):
-            pd.plotting.scatter_matrix(stockData[["firstDiffAbs","newsRating"]])
-            pd.plotting.scatter_matrix(stockData[["adjusted close","newsRating"]])
-        plt.show()
 
     # normalize data
     if _normalize:        
@@ -221,10 +206,41 @@ def process_stock(_stock="AAPL", _print=False, _type="compact",freq="daily" ,_lo
 
     return stockData
 # =======================================
+def addNews(file="AAPL/weekly_adjusted_AAPL_processed.csv", _loadnews=True,_print=False):
+    stockData = pd.read_csv(file, parse_dates=['timestamp'])
+    stockData.set_index('timestamp', inplace=True)
+    # check if news information shall be loaded for the stock
+    if(_loadnews):
+        # load apple news nyt and set index
+        News = load_file("AAPL/NYT/newsinfo.csv","date begin")
+        News.set_index('date begin',inplace=True)
+
+        if(_print): 
+            # print news classification
+            plot_bargraph(News.index, News['score'],"News Score", "yearly")
+
+        # add news rating as colum to stockdata
+        stockData['newsRating'] = News['score']
+        # stockData += News.pos_simple
+
+        stockData.info()
+        if(_print):
+            stockData.corr() # see and plot correlations 
+            pd.plotting.scatter_matrix(stockData[["firstDiffAbs","newsRating"]])
+            pd.plotting.scatter_matrix(stockData[["adjusted close","newsRating"]])
+            plt.show()
+    stockData.to_csv("AAPL/weekly_adjusted_AAPL_processed_final.csv")
+
 # data = process_stock(_print=False,_loadnews=True)
-process_stock(freq="weekly",_normalize=False)
+# process_stock(freq="weekly",_normalize=False)
 # training(file="AAPL/stockData.csv")
 # training(file="AAPL/StockFull.csv")
 training(file="AAPL/weekly_adjusted_AAPL_corr.csv")
 
-model_nn = create_nn_model(shape, neurons, d)
+# model_nn = create_nn_model(shape, neurons, d)
+
+# newsNYT = pd.read_csv("AAPL/NYT/newsinfo.csv", parse_dates=['date begin'])
+# newsNYT.set_index('date begin',inplace=True)
+# newsNYT.info()
+
+# addNews()
